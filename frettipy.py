ea7231b5bbb7d7ca628bebcab3576abe9ca0ce87
Jan Berges <jan.berges@uni-bremen.de>
2024-05-24 15:12:06 +0200

    Increment version number

--- frettipy.py
+++ frettipy.py
@@ -10,7 +10,7 @@ control. Otherwise the intended modifications are shown without changing `FILE`.
 If `FILE` is a directory, **all .py files in the directory tree are processed!**
 """
 
-__version__ = '0.3'
+__version__ = '0.4'
 
 import os
 import re
